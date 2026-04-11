63efb06ddc79df3a64b79b3477390a029c29f02b
servicesmrben-wq <services.mrben@gmail.com>
2026-04-11 08:59:42 -0400

    Update layout.tsx

--- mrben-site1/app/layout.tsx
+++ mrben-site1/app/layout.tsx
@@ -20,6 +20,27 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script
+          data-cookieconsent="ignore"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag("consent", "default", {
+                  ad_personalization: "denied",
+                  ad_storage: "denied",
+                  ad_user_data: "denied",
+                  analytics_storage: "denied",
+                  functionality_storage: "denied",
+                  personalization_storage: "denied",
+                  security_storage: "granted",
+                  wait_for_update: 500,
+              });
+              gtag("set", "ads_data_redaction", true);
+              gtag("set", "url_passthrough", false);
+            `
+          }}
+        />
         <Script
           id="Cookiebot"
           src="https://consent.cookiebot.com/uc.js"
